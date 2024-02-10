14cfc025360faab9e338fe14b7d7efe69d60b0fc
JoyRume <joy.rume@student.moringaschool.com>
2024-02-10 10:44:36 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,30 @@ import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "A River Runs Through It" },
-    { id: 2, title: "Se7en" },
-    { id: 3, title: "Inception" }
-  ]);
+  const [movies, setMovies] = useState({
+    1: { id: 1, title: "A River Runs Through It" },
+    2: { id: 2, title: "Se7en" },
+    3: { id: 3, title: "Inception" },
+  });
+
+  // Function to add a new movie
+  const addMovie = (id, title) => {
+    
+    const newMovie = { id, title };
+    // Updating the movies state with the new movie
+    setMovies((prevMovies) => {
+      // Using spread operator to copy the previous movies object
+      return { ...prevMovies, [id]: newMovie };
+    });
+  };
 
   return (
     <div>
       <NavBar />
       <Switch>
         <Route path="/movies">
-          <MoviesPage movies={movies} />
+          {/* Passing addMovie function to MoviesPage component */}
+          <MoviesPage movies={movies} addMovie={addMovie} />
         </Route>
         <Route exact path="/">
           <div>Home</div>
